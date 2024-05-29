1f8866d8526fd8adbc900306196156e64c7d36f8
Rohithmatham12 <81304757+Rohithmatham12@users.noreply.github.com>
2024-05-29 10:21:00 -0700

    Initial commit

---
+++ commands.sh
@@ -0,0 +1,49 @@
+## Wrote new keypair to C:\Users\user\OneDrive\Desktop\Assignment\Owner.json 
+#==========================================================================
+# #pubkey: 5FLAArMTTHTTarxFs8YxxSqiDfZHGberPXGM336MMfu9
+# ==========================================================================
+# Save this seed phrase and your BIP39 passphrase to recover your new keypair:
+# you mirror long major popular trick gasp sister true express rebel defense
+
+#############################################################################
+
+# Wrote new keypair to C:\Users\user\OneDrive\Desktop\Assignment\Creator.json
+# ==============================================================================
+# pubkey: C7UpqNrkzgW5PfEtT135NMGYU6Y7DXV1A4SdSDezQ2b5
+# ==============================================================================
+# Save this seed phrase and your BIP39 passphrase to recover your new keypair:
+# shift female phone benefit wheat recall deny venue nature verb captain bargain
+# ==============================================================================
+
+#################################################################################
+
+solana config set --url https://metaplex.devnet.rpcpool.com
+
+#funding the wallet
+
+solana airdrop 1 6pkEgvrCyYCcAnEDiQrxdMnUGiQWRfHrmemEHr71U7wF --url https://api.devnet.solana.com
+
+
+
+##stopped at adding value to the wallet on vhrome
+
+
+# Created edition: 1, with DNA: d47b0a25b0dda3139a44cbb2d27d8975026f7683
+# Created edition: 2, with DNA: 091d1c319c862444e4a361144387ba5d73f6c64d
+# Created edition: 3, with DNA: cfa27eeba852b5a1cc1b861892aa36c65b2df024
+# Created edition: 4, with DNA: 01f773ac1b5a67929370a6ff1ed5cef02cf13625
+# Created edition: 5, with DNA: 62d9e30798ab3cbe6a6366f26ac17e68cc322cc1
+
+
+##Sugar command to create the Cnfig file
+./sugar 
+
+
+###NFT Storgae
+6570b54b.60e1c978efa54d109a70fd53d7448aeb
+
+
+
+###Candy mahine ID
+
+Ed2Mbb7EnGNVmFBjgkhYL3hRiFfwMLTtGfodGo6BHeKu
